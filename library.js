a65b629e6d15a46a49299f576e210799bc5ca257
Chase Vickery <chasedvickery@gmail.com>
2025-03-25 11:14:17 -0400

    add and display books

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const library = [];
+const libraryCards = [];
 
 // START Book
 function Book(title, author, numPages, read) {
@@ -28,3 +29,40 @@ addBookToLibrary("Another Title", "Sally", 456, true);
 addBookToLibrary("My book 2", "Bob", 167, false);
 
 console.log(library);
+
+
+// DOM organization
+const libraryDisplay = document.querySelector(".library");
+const bookDisplayTemplate = document.querySelector("#book-card-base");
+bookDisplayTemplate.remove();
+bookDisplayTemplate.id = null;
+
+function displayCard(card, book) {
+    const titleElement = card.querySelector(".title");
+    const authorElement = card.querySelector(".author");
+
+    titleElement.textContent = book.title;
+    authorElement.textContent = book.author;
+}
+function removeLastCard() {
+    libraryCards.removeChild(libraryDisplay.lastChild);
+    libraryDisplay.removeChild(libraryDisplay.lastChild);
+}
+function addNewCardDisplay() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node/cloneNode
+    const newDisplay = bookDisplayTemplate.cloneNode(true);
+    libraryDisplay.appendChild(newDisplay);
+    libraryCards.push(newDisplay);
+}
+
+function displayLibrary() {
+    // Correct the number of displays
+    while (libraryCards.length > library.length) { removeLastCardDisplay(); }
+    while (libraryCards.length < library.length) { addNewCardDisplay(); }
+    // Modify all card content to match library
+    for (let i = 0; i < library.length; i++) {
+        displayCard(libraryCards[i], library[i]);
+    }
+}
+
+displayLibrary();
